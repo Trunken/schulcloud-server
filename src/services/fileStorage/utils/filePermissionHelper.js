53607292a8d7a198f655f610e330c0d931557ce3
Robert Weber <closingtag@gmail.com>
2019-01-31 21:54:04 +0100

    Implement role sorting

--- src/services/fileStorage/utils/filePermissionHelper.js
+++ src/services/fileStorage/utils/filePermissionHelper.js
@@ -1,5 +1,7 @@
-const {FileModel} = require('../model');
+const { FileModel } = require('../model');
 const { userModel } = require('../../user/model');
+const RoleModel = require('../../role/model');
+const { sortRoles } = require('../../role/utils/rolesHelper');
 
 const getFile = (id) => {
 	return FileModel
@@ -9,19 +11,19 @@ const getFile = (id) => {
 };
 
 const checkPermissions = (permission) => {
-	return async (user, file) => {		
+	return async (user, file) => {
 		const fileObject = await getFile(file);
 		const {
 			permissions,
 			refOwnerModel,
 			owner: { _id: owner }
 		} = fileObject;
-		
+
 		// return always true for owner of file
 		if( user.toString() === owner.toString() ) {
 			return Promise.resolve(true);
 		}
-		
+
 		// or legacy course model
 		if( refOwnerModel === 'course' ) {
 			const userObject = await userModel.findOne({_id: user}).populate('roles').exec();
@@ -38,26 +40,61 @@ const checkPermissions = (permission) => {
 
 		const teamMember = fileObject.owner.userIds.find(_ => _.userId.toString() === user.toString());
 		const userPermissions = permissions.find(perm => perm.refId.toString() === user.toString());
-		
+
 		// User is either not member of Team
 		// or file has no explicit user permissions (sharednetz files)
-		if(!teamMember && !userPermissions) {			
+		if(!teamMember && !userPermissions) {
 			return Promise.reject();
 		}
 
-		return new Promise((resolve, reject) => {
+		if( userPermissions ) {
+			return userPermissions[permission] ? Promise.resolve(true) : Promise.reject();
+		}
 
-			if( userPermissions ) {
-				return userPermissions[permission] ? resolve(true) : reject();
-			}
-
-			const { role } = teamMember;
-			const rolePermissions = permissions.find(perm => perm.refId.toString() === role.toString());
-
-			return rolePermissions[permission] ? resolve(true) : reject();
+		return checkTeamPermission({
+			permission,
+			file: fileObject,
+			user: teamMember
 		});
 	};
 };
+
+const checkTeamPermission = async ({ user, file, permission}) => {
+
+	let teamRoles;
+
+	try {
+		teamRoles = sortRoles(await RoleModel.find({ name: /^team/ }).exec());
+	} catch (error) {
+		console.log(error);
+	}
+
+	return new Promise((resolve, reject) => {
+
+		const { role } = user;
+		const { permissions } = file;
+		const rolePermissions = permissions.find(perm => perm.refId.toString() === role.toString());
+
+		const { role: creatorRole } = file.owner.userIds.find(_ => _.userId.toString() === file.permissions[0].refId.toString());
+
+		const findRole = (roleId) => {
+			return (roles) => {
+				return roles.findIndex(role => role._id.toString() === roleId.toString()) > -1;
+			};
+		};
+
+		if (permission === 'delete') {
+
+			const userPos = teamRoles.findIndex(findRole(role));
+			const creatorPos = teamRoles.findIndex(findRole(creatorRole));
+
+			return userPos > creatorPos ? resolve(true) : reject();
+		}
+
+		return rolePermissions[permission] ? resolve(true) : reject();
+	});
+};
+
 
 module.exports = {
 	checkPermissions,
